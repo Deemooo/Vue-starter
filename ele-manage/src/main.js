c0fb827bfc0c48aa55f116357d1b51efd20921b1
Deemooo <yibiluo@foxmail.com>
2018-08-10 17:05:32 +0800

    引入iview

--- ele-manage/src/main.js
+++ ele-manage/src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+// 引入iview
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+// import './assets/style/myTheme/index.less';
 
-Vue.config.productionTip = false
+Vue.use(iView);
 
+Vue.config.productionTip = false;
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- ele-manage/src/views/login.vue
+++ ele-manage/src/views/login.vue
@@ -1,6 +1,22 @@
 <template>
-    <div class="page-warp">
-        123
+    <div class="login-page">
+        <div class="login-page-form-wrap">
+            <Form ref="formInline" :model="formInline" :rules="ruleInline">
+                <FormItem prop="user">
+                    <Input type="text" v-model="formInline.user" placeholder="Username">
+                    <Icon type="ios-person-outline" slot="prepend"></Icon>
+                    </Input>
+                </FormItem>
+                <FormItem prop="password">
+                    <Input type="password" v-model="formInline.password" placeholder="Password">
+                    <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                    </Input>
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
+                </FormItem>
+            </Form>
+        </div>
     </div>
 </template>
 <script>
@@ -8,14 +24,47 @@ export default {
     components: {},
     computed: {},
     data () {
-        return {};
+        return {
+            formInline: {
+                user: '',
+                password: ''
+            },
+            ruleInline: {
+                user: [
+                    { required: true, message: 'Please fill in the user name', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: 'Please fill in the password.', trigger: 'blur' },
+                    { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
+                ]
+            }
+        };
     },
-    methods: {},
+    methods: {
+        handleSubmit (name) {
+            this.$refs[name].validate((valid) => {
+                if (valid) {
+                    this.$Message.success('Success!');
+                } else {
+                    this.$Message.error('Fail!');
+                }
+            })
+        }
+    },
     mounted () {
     },
     watch: {}
 };
 </script>
 <style lang="less" scoped>
-
+ .login-page {
+     width: 100%;
+     height: 100%;
+     .login-page-form-wrap {
+         width: 600px;
+         height: 400px;
+         margin: auto;
+         margin-top: 200px;
+     }
+ }
 </style>
