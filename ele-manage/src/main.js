cd893cb2f50bd8996de3b741d77b9f12330b0a24
Deemooo <yibiluo@foxmail.com>
2018-08-13 00:07:25 +0800

    因为vue-cli 3.0的配置不便，重新基于vue-cli 2.0构建项目

--- ele-manage/src/main.js
+++ ele-manage/src/main.js
@@ -1,17 +1,18 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './App';
 import router from './router';
-import store from './store';
 // 引入iview
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
-// import './assets/style/myTheme/index.less';
+import './assets/style/myTheme/index.less';
+Vue.config.productionTip = false;
 
 Vue.use(iView);
 
-Vue.config.productionTip = false;
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  store,
-  render: h => h(App)
-}).$mount('#app');
+  components: { App },
+  template: '<App/>'
+});
---
+++ ele-manage/src/pages/login.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="login-page">
+      <div class="login-page-title">ele后台管理系统</div>
+      <div class="login-page-form-wrap">
+          <Form ref="formInline" :model="formInline" :rules="ruleInline">
+              <FormItem prop="user">
+                  <Input type="text" v-model="formInline.user" placeholder="Username">
+                  <Icon type="ios-person-outline" slot="prepend"></Icon>
+                  </Input>
+              </FormItem>
+              <FormItem prop="password">
+                  <Input type="password" v-model="formInline.password" placeholder="Password">
+                  <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                  </Input>
+              </FormItem>
+              <FormItem>
+                  <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
+              </FormItem>
+          </Form>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+    components: {},
+    computed: {},
+    data () {
+        return {
+            formInline: {
+                user: '',
+                password: ''
+            },
+            ruleInline: {
+                user: [
+                    { required: true, message: 'Please fill in the user name', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: 'Please fill in the password.', trigger: 'blur' },
+                    { type: 'string', min: 6, message: 'The password length cannot be less than 6 bits', trigger: 'blur' }
+                ]
+            }
+        };
+    },
+    methods: {
+        handleSubmit (name) {
+            this.$refs[name].validate((valid) => {
+                if (valid) {
+                    this.$Message.success('Success!');
+                } else {
+                    this.$Message.error('Fail!');
+                }
+            })
+        }
+    },
+    mounted () {},
+    watch: {}
+};
+</script>
+<style lang="less" scoped>
+  .login-page {
+    width: 100%;
+    height: 100%;
+    background-color: #515a6e;
+    .login-page-title {
+      position: relative;
+      top: 50px;
+      font-size: 25px;
+      color: #f8f8f9;
+    }
+    .login-page-form-wrap {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-top: -105px;
+      margin-left: -160px;
+      box-sizing: border-box;
+      width: 320px;
+      height: 210px;
+      padding: 35px;
+      background-color: #f8f8f9;
+      border-radius: 4px;
+    }
+  }
+</style>
