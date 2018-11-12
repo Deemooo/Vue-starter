5c9e7db8476b60b1287cc2c2d16c9de327357fde
Deemooo <yibiluo@foxmail.com>
2018-11-13 00:11:53 +0800

    完善profile页

--- vue-ele/src/pages/subpages/benefit.vue
+++ vue-ele/src/pages/subpages/benefit.vue
@@ -14,60 +14,88 @@
     </div>
     <div v-show="benefitType === 0">
       <div class="current-benefit-head">
-        <span class="head-title">有 {{ benefitList.length }}个红包即将到期</span>
+        <span class="head-title">有 <span>{{ benefitList.length }}</span>个红包即将到期</span>
         <router-link tag="span" to="benefitDetail" class="benefit-explain">
           <img src="../../assets/icon/description.png">
           <span>红包说明</span>
         </router-link>
       </div>
-      <div class="benefit-item-wrap">
-        <div class="benefit-item-left">
-          <div class="benefit-item-left-money">
-            <span>¥</span>
-            <span class="number">1</span>
-            <span>.0</span>
-          </div>
-          <div class="benefit-item-left-condition">满 20 元可用</div>
-        </div>
-        <div class="benefit-item-right">
-          <div class="benefit-item-right-item">
-            <div class="share-benefit">分享红包</div>
-            <div class="benefit-item-right-condition">
-              <div>2017-05-23到期</div>
-              <div>限收货手机号为 13681711254</div>
+      <div class="benefit-wrap" v-for="item in benefitList" :key="item.id">
+        <div class="benefit-item-wrap">
+          <div class="benefit-item-left">
+            <div class="benefit-item-left-money">
+              <span>¥</span>
+              <span class="number">{{ String(item.amount).split('.')[0] }}</span>
+              <span>.</span>
+              <span>{{ String(item.amount).split('.')[1]||0 }}</span>
             </div>
+            <div class="benefit-item-left-condition">{{ item.description_map.sum_condition }}</div>
           </div>
-          <div class="benefit-item-right-date">剩3日</div>
+          <div class="benefit-item-right">
+            <div class="benefit-item-right-item">
+              <div class="share-benefit">{{ item.name }}</div>
+              <div class="benefit-item-right-condition">
+                <div>{{ item.description_map.validity_periods }}</div>
+                <div>{{ item.description_map.phone }}</div>
+              </div>
+            </div>
+            <div class="benefit-item-right-date">{{item.description_map.validity_delta}}</div>
+          </div>
         </div>
+        <div v-if="item.limit_map" class="benefit-item-limit">{{ item.limit_map.restaurant_flavor_ids }}</div>
       </div>
     </div>
     <div v-show="benefitType === 1">
-      <div class="current-benefit-head">
-        <router-link tag="span" to="couponDetail" class="benefit-explain">
+      <div class="coupon-detail-head">
+        <router-link tag="span" to="couponDetail" class="coupon-explain">
           <img src="../../assets/icon/description.png">
           <span>商家代金券说明</span>
         </router-link>
       </div>
-      <div class="benefit-item-wrap">
-
+      <div class="coupon-detail">
+        <img src="../../assets/icon/voucher.png">
+        <div class="no-record">无法使用代金券~</div>
+        <div class="no-record-explain">非客户端或客户端版本过低</div>
+        <router-link to="/download" tag="span" class="download-app">
+          下载或升级客户端
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 <script>
+  import { mapState } from 'vuex';
   export default {
     components: {},
-    computed: {},
+    computed: {
+      ...mapState([
+        'userInfo'
+      ])
+    },
     data () {
       return {
         benefitType: 0,
         benefitList: []
       };
     },
-    methods: {},
-    mounted () {
+    methods: {
+      getBenefitList () {
+        if (this.userInfo) {
+          this.https({url: '/promotion/v2/users/' + this.userInfo.id + '/hongbaos?limit=20&offset=0', method: 'get'}).then(
+            (res) => {
+              this.benefitList = res;
+            });
+        }
+      }
     },
-    watch: {}
+    mounted () {
+      this.getBenefitList();
+    },
+    watch: {
+      userInfo () {
+        this.getBenefitList();
+      }
+    }
   };
 </script>
 <style lang="less" scoped>
@@ -121,7 +149,10 @@
         font-size: .55rem;
       }
       .head-title {
-        color: @fontColor2;
+        color: @fontColor1;
+        span {
+          color: @fontColor4;
+        }
       }
       .benefit-explain {
         display: flex;
@@ -136,57 +167,121 @@
         }
       }
     }
-    .benefit-item-wrap {
+    .coupon-detail-head {
       display: flex;
+      justify-content: flex-end;
       align-items: center;
-      background: url(../../assets/icon/hongbao.png) repeat-x ;
-      background-size: .5rem .2rem;
-      padding: .8rem .4rem;
-      margin: 0 .5rem;
-      box-shadow: .1rem .1rem .025rem .1rem @backColor;
-      .benefit-item-left {
-        flex: 0 0 20%;
-        border-right: .025rem dotted @gray;
-        .benefit-item-left-money {
-          word-spacing: -.4rem;
-          span {
-            font-size: .75rem;
-            color: @fontColor4;
-            font-weight: 700;
-          }
-          .number {
-            font-size: 1.5rem;
-            color: @fontColor4;
-          }
+      padding: .4rem;
+      background-color: #fff;
+      span {
+        font-size: .55rem;
+      }
+      .coupon-explain {
+        display: flex;
+        align-items: center;
+        img {
+          width: .6rem;
+          height: .6rem;
+          margin-right: .2rem;
         }
-        .benefit-item-left-condition {
-          font-size: .4rem;
-          color: @fontColor2;
+        span {
+          color: @blue;
         }
       }
-      .benefit-item-right {
-        flex: 0 0 70%;
+    }
+    .benefit-wrap {
+      margin: .2rem 0;
+      .benefit-item-wrap {
         display: flex;
-        justify-content: space-around;
         align-items: center;
-        .benefit-item-right-item {
-          margin-left: 1rem;
-          .share-benefit {
-            font-size: .7rem;
-            color: @fontColor1;
-          }
-          .benefit-item-right-condition {
-            font-size: .4rem;
-            div {
-              color: @fontColor2;
+        background: url(../../assets/icon/hongbao.png) repeat-x ;
+        background-size: .5rem .2rem;
+        padding: .8rem .4rem;
+        margin: 0 .5rem;
+        box-shadow: .1rem .1rem .025rem .1rem @backColor;
+        .benefit-item-left {
+          flex: 0 0 20%;
+          border-right: .025rem dotted @gray;
+          .benefit-item-left-money {
+            word-spacing: -.4rem;
+            span {
+              font-size: .75rem;
+              color: @fontColor4;
+              font-weight: 700;
+            }
+            .number {
+              font-size: 1.5rem;
+              color: @fontColor4;
             }
           }
+          .benefit-item-left-condition {
+            font-size: .4rem;
+            color: @fontColor2;
+          }
         }
-        .benefit-item-right-date {
-          margin-left: .4rem;
-          font-size: .75rem;
-          color: @fontColor4;
+        .benefit-item-right {
+          flex: 0 0 70%;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          .benefit-item-right-item {
+            margin-left: 1rem;
+            .share-benefit {
+              font-size: .7rem;
+              color: @fontColor1;
+            }
+            .benefit-item-right-condition {
+              font-size: .4rem;
+              div {
+                color: @fontColor2;
+              }
+            }
+          }
+          .benefit-item-right-date {
+            margin-left: .4rem;
+            font-size: .75rem;
+            color: @fontColor4;
+          }
         }
+      }
+      .benefit-item-limit {
+        padding: .4rem;
+        font-size: .4rem;
+        color: @fontColor2;
+        text-align: center;
+      }
+    }
+    .coupon-detail {
+      text-align: center;
+      background-color: #fff;
+      .division {
+        padding: .4rem;
+        text-align: left;
+        font-size: .5rem;
+        color: @fontColor1;
+      }
+      img {
+        width: 8rem;
+        height: 5rem;
+      }
+      .no-record {
+        text-align: center;
+        color: @fontColor1;
+        font-size: .7rem;
+      }
+      .no-record-explain {
+        font-size: .5rem;
+        color: @fontColor2;
+        margin-top: .3rem;
+        margin-bottom: .3rem;
+      }
+      .download-app {
+        display: inline-block;
+        padding: .3rem;
+        border-radius: .15rem;
+        background-color: @blue;
+        font-size: .65rem;
+        color: #fff;
       }
     }
   }
