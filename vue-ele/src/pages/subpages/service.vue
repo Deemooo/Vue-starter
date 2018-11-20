48537ebbf7dcca264ac14f0c1e1405d28a9f5236
Deemooo <yibiluo@foxmail.com>
2018-11-20 23:47:07 +0800

    完善profile页

--- vue-ele/src/pages/subpages/service.vue
+++ vue-ele/src/pages/subpages/service.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="page-warp">
-      service
+    <div class="service">
+      <top-header>
+        <template>
+          <svg class="arrow-left" xmlns="http://www.w3.org/2000/svg" version="1.1" @click="$router.go(-1)">
+            <polyline points="12,18 4,9 12,0" style="fill:none;stroke:rgb(255,255,255);stroke-width:2"/>
+          </svg>
+          <span class="service-title">会员中心</span>
+        </template>
+      </top-header>
     </div>
 </template>
 <script>
@@ -17,4 +24,27 @@
   };
 </script>
 <style lang="less" scoped>
+  @import (reference) "../../assets/style/dynamic";
+  .service {
+    width: 100%;
+    overflow-y: auto;
+    position: relative;
+    color: #fff;
+    svg {
+      box-sizing: border-box;
+    }
+    .arrow-left {
+      margin-left: .4rem;
+      flex: 0 0 33.333%;
+      height: .8rem;
+    }
+    .service-title {
+      flex: 0 0 33.333%;
+      font-size: .8rem;
+      line-height: .8rem;
+      text-align: center;
+      font-weight: 700;
+      color: #fff;
+    }
+  }
 </style>
